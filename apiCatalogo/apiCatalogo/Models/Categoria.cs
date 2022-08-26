d9d1a694d03985e1955617cd7d8129e7dd77bdd6
Thiago jose <thiago.ads.silva@gmail.com>
2022-08-25 21:07:56 -0300

    Criação do projeto e as classes modelos

---
+++ apiCatalogo/apiCatalogo/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace apiCatalogo.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }     
+        public string Nome { get; set; }
+        public string Descricao { get; set; } 
+        public string ImagemUrl { get; set; }
+        public string Status { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
