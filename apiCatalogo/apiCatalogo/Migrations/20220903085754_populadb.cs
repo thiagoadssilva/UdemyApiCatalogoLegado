9c30cdea311689833d6c8b5183393ee4fdec0b72
Thiago jose <thiago.ads.silva@gmail.com>
2022-09-03 06:19:30 -0300

    Criando as informações das tabelas usando migration

---
+++ apiCatalogo/apiCatalogo/Migrations/20220903085754_populadb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apiCatalogo.Migrations
+{
+    public partial class populadb : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO apicatalogodb.categorias (Nome, ImagemUri) VALUES('Bebidas', 'bebidas.jpg')");
+            mb.Sql("INSERT INTO apicatalogodb.categorias (Nome, ImagemUri) VALUES('Lanches', 'lanches.jpg')");
+            mb.Sql("INSERT INTO apicatalogodb.categorias (Nome, ImagemUri) VALUES('Sobremesas', 'sobremesas.jpg')");
+
+            mb.Sql("INSERT INTO apicatalogodb.produtos (Nome, Descricao, Preco, ImagemUri, Estoque, DataCadastro, CategoriaId) VALUES('Coca-cola', 'Refrigerante', 50, 'cocacola.jpg', 50, now(), " +
+                "(select CategoriaId  from categorias where Nome = 'Bebidas'))");
+            mb.Sql("INSERT INTO apicatalogodb.produtos (Nome, Descricao, Preco, ImagemUri, Estoque, DataCadastro, CategoriaId) VALUES('Bolo', 'Doce', 55, 'bolo.jpg', 10, now(), " +
+                "(select CategoriaId  from categorias where Nome = 'Lanches'))");
+            mb.Sql("INSERT INTO apicatalogodb.produtos (Nome, Descricao, Preco, ImagemUri, Estoque, DataCadastro, CategoriaId) VALUES('Sorvete', 'muito bom', 25, 'sorvete.jpg', 15, now(), " +
+                "(select CategoriaId  from categorias where Nome = 'Sobremesas'))");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM produtos");
+            mb.Sql("DELETE FROM categorias");
+        }
+    }
+}
