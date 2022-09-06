7a0c4d55b18afbd8d959fa688670ff98b65bd576
Thiago jose <thiago.ads.silva@gmail.com>
2022-09-06 06:52:58 -0300

    Varificar o erro do endpoint get

---
+++ apiCatalogo/apiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,23 @@
+﻿using apiCatalogo.Context;
+using apiCatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiCatalogo.Controllers
+{
+    [Route("api/[Controller]")] 
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;    
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produtos.ToList();
+        }
+    }
+}
