5575e9c98b843cf2457af5d4d1e2328cbe59c256
Thiago jose <thiago.ads.silva@gmail.com>
2022-09-07 18:29:55 -0300

    Populando as tabelas

--- apiCatalogo/apiCatalogo/Migrations/20220903085754_populadb.cs
+++ apiCatalogo/apiCatalogo/Migrations/20220903085754_populadb.cs
@@ -8,15 +8,15 @@ namespace apiCatalogo.Migrations
     {
         protected override void Up(MigrationBuilder mb)
         {
-            mb.Sql("INSERT INTO apicatalogodb.categorias (Nome, ImagemUri) VALUES('Bebidas', 'bebidas.jpg')");
-            mb.Sql("INSERT INTO apicatalogodb.categorias (Nome, ImagemUri) VALUES('Lanches', 'lanches.jpg')");
-            mb.Sql("INSERT INTO apicatalogodb.categorias (Nome, ImagemUri) VALUES('Sobremesas', 'sobremesas.jpg')");
+            mb.Sql("INSERT INTO apicatalogodb01.categorias (Nome, ImagemUri) VALUES('Bebidas', 'bebidas.jpg')");
+            mb.Sql("INSERT INTO apicatalogodb01.categorias (Nome, ImagemUri) VALUES('Lanches', 'lanches.jpg')");
+            mb.Sql("INSERT INTO apicatalogodb01.categorias (Nome, ImagemUri) VALUES('Sobremesas', 'sobremesas.jpg')");
 
-            mb.Sql("INSERT INTO apicatalogodb.produtos (Nome, Descricao, Preco, ImagemUri, Estoque, DataCadastro, CategoriaId) VALUES('Coca-cola', 'Refrigerante', 50, 'cocacola.jpg', 50, now(), " +
+            mb.Sql("INSERT INTO apicatalogodb01.produtos (Nome, Descricao, Preco, ImagemUri, Estoque, DataCadastro, CategoriaId) VALUES('Coca-cola', 'Refrigerante', 50, 'cocacola.jpg', 50, now(), " +
                 "(select CategoriaId  from categorias where Nome = 'Bebidas'))");
-            mb.Sql("INSERT INTO apicatalogodb.produtos (Nome, Descricao, Preco, ImagemUri, Estoque, DataCadastro, CategoriaId) VALUES('Bolo', 'Doce', 55, 'bolo.jpg', 10, now(), " +
+            mb.Sql("INSERT INTO apicatalogodb01.produtos (Nome, Descricao, Preco, ImagemUri, Estoque, DataCadastro, CategoriaId) VALUES('Bolo', 'Doce', 55, 'bolo.jpg', 10, now(), " +
                 "(select CategoriaId  from categorias where Nome = 'Lanches'))");
-            mb.Sql("INSERT INTO apicatalogodb.produtos (Nome, Descricao, Preco, ImagemUri, Estoque, DataCadastro, CategoriaId) VALUES('Sorvete', 'muito bom', 25, 'sorvete.jpg', 15, now(), " +
+            mb.Sql("INSERT INTO apicatalogodb01.produtos (Nome, Descricao, Preco, ImagemUri, Estoque, DataCadastro, CategoriaId) VALUES('Sorvete', 'muito bom', 25, 'sorvete.jpg', 15, now(), " +
                 "(select CategoriaId  from categorias where Nome = 'Sobremesas'))");
         }
 
---
+++ apiCatalogo/apiCatalogo/Migrations/20220907211313_migracaoInicialBancoDois.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace apiCatalogo.Migrations
+{
+    public partial class migracaoInicialBancoDois : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO apialunosdb01.categorias (Nome, ImagemUrl) VALUES('Bebidas', 'bebidas.jpg')");
+            mb.Sql("INSERT INTO apialunosdb01.categorias (Nome, ImagemUrl) VALUES('Lanches', 'lanches.jpg')");
+            mb.Sql("INSERT INTO apialunosdb01.categorias (Nome, ImagemUrl) VALUES('Sobremesas', 'sobremesas.jpg')");
+
+            mb.Sql("INSERT INTO apialunosdb01.produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) VALUES('Coca-cola', 'Refrigerante', 50, 'cocacola.jpg', 50, now(), " +
+                "(select CategoriaId  from categorias where Nome = 'Bebidas'))");
+            mb.Sql("INSERT INTO apialunosdb01.produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) VALUES('Bolo', 'Doce', 55, 'bolo.jpg', 10, now(), " +
+                "(select CategoriaId  from categorias where Nome = 'Lanches'))");
+            mb.Sql("INSERT INTO apialunosdb01.produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) VALUES('Sorvete', 'muito bom', 25, 'sorvete.jpg', 15, now(), " +
+                "(select CategoriaId  from categorias where Nome = 'Sobremesas'))");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM produtos");
+            mb.Sql("DELETE FROM categorias");
+        }
+    }
+}
